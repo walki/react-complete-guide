b8fa154af5c548ffbab5962a98c2bef0e7be4fc2
Roger Walker <roger.john.walker@gmail.com>
2023-02-07 20:11:13 -0500

    ch 54 - slight refactor

--- react-comp-guide/src/components/NewExpense/ExpenseForm.js
+++ react-comp-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 export default function ExpenseForm() {
-
-  const [ expenseData, setExpenseData ] = useState({ title: '', amount: '0.00', date: '2019-01-01'});
+  const [expenseData, setExpenseData] = useState({
+    title: "",
+    amount: "0.00",
+    date: "2019-01-01",
+  });
 
   const changeHandler = (event) => {
 
-    if (event.target.id === 'expense-title'){
-      expenseData.title = event.target.value;
-    } else if (event.target.id === 'expense-amount') {
-      expenseData.amount = event.target.value;
-    } else if (event.target.id === 'expense-date') {
-      expenseData.date = event.target.value;
+    let newData = { ...expenseData };
+
+    if (event.target.id === "expense-title") {
+      newData.title = event.target.value;
+    } else if (event.target.id === "expense-amount") {
+      newData.amount = event.target.value;
+    } else if (event.target.id === "expense-date") {
+      newData.date = event.target.value;
     }
 
-    setExpenseData(expenseData);
-    console.log(expenseData);
-
-  }
+    setExpenseData(newData);
+    
+    console.log(newData);
+  };
 
   return (
     <>
@@ -26,15 +31,27 @@ export default function ExpenseForm() {
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" id="expense-title" onChange={changeHandler}/>
+            <input type="text" id="expense-title" onChange={changeHandler} />
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" id='expense-amount' min="0.01" step="0.01" onChange={changeHandler}/>
+            <input
+              type="number"
+              id="expense-amount"
+              min="0.01"
+              step="0.01"
+              onChange={changeHandler}
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" id='expense-date' min="2019-01-01" max="2025-12-31" onChange={changeHandler}/>
+            <input
+              type="date"
+              id="expense-date"
+              min="2019-01-01"
+              max="2025-12-31"
+              onChange={changeHandler}
+            />
           </div>
         </div>
         <div className="new-expense__actions">

4a676812477666f5015c0762b06838ca8e2803e3
Roger Walker <roger.john.walker@gmail.com>
2023-02-18 12:05:19 -0500

    ch 59 -assignment 2-  state and events

--- react-comp-guide/src/components/Expenses/Expenses.js
+++ react-comp-guide/src/components/Expenses/Expenses.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 function Expenses(props) {
+
+  const [filterYear, setFilterYear] = useState('2025');
+
+  const filterYearChangeHandler = (year) => {
+
+    setFilterYear(year);
+
+    console.log(filterYear);
+  }
+
   return (
-    <Card className="expenses">
-      {props.items.map((exp) => (
-        <ExpenseItem title={exp.title} date={exp.date} amount={exp.amount} />
-      ))}
-    </Card>
+    <div>
+      <ExpensesFilter onYearChange={filterYearChangeHandler}>Year: {filterYear}</ExpensesFilter>
+      <Card className="expenses">
+        {props.items.map((exp) => (
+          <ExpenseItem title={exp.title} date={exp.date} amount={exp.amount} />
+        ))}
+      </Card>
+    </div>
   );
 }
 

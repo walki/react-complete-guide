52a1e98986ed7c6c0511c2eb04fad551da2e83e4
Roger Walker <roger.john.walker@gmail.com>
2023-02-20 20:36:01 -0500

    moving reset to text input onChange

--- react-comp-guide2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-comp-guide2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,6 +8,9 @@ const CourseInput = props => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -18,7 +21,6 @@ const CourseInput = props => {
       return;
     }
 
-    setIsValid(true);
     props.onAddGoal(enteredValue);
   };
 
